59889b4e20d2dd06b12277b33346d1d726c05c41
Zach Bennett <xslingshot@gmail.com>
2012-07-14 17:45:37 -0400

    exported tileset tsx files

---
+++ data/tilesets/grass 16x16.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="grass 16x16" tilewidth="16" tileheight="16">
+ <image source="grass-tileset.png" width="960" height="960"/>
+</tileset>
---
+++ data/tilesets/helios zelda.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="helios zelda" tilewidth="16" tileheight="16">
+ <image source="helios-zelda-snes.png" width="1550" height="930"/>
+</tileset>
